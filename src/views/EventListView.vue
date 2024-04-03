b3b7a38221a1e334d47a35a17d6613cd053b94f6
Owen Chen <73706094+Spxxk@users.noreply.github.com>
2024-04-02 20:08:33 -0400

    Frontend

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -17,7 +17,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <h1>Events For Good</h1>
+  <h1>Teams</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
